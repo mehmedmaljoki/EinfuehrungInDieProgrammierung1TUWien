9a1257690d199f25669bc3234e0dbe9aaa114d11
Mehmed Maljoki <mehmed.maljoki@porscheinformatik.com>
2022-09-01 00:44:57 +0200

    adding the addSeparator method to Aufgabe2.java

--- src/Aufgabe2.java
+++ src/Aufgabe2.java
@@ -5,9 +5,24 @@ public class Aufgabe2 {
 
     private static void addSeparator(String text, char separator) {
         //TODO: Implementieren Sie hier Ihre Lösung für die Angabe
-
-
+        String copy = text;
+        if (text.length() == 1) {
+            System.out.println(text);
+        } else if (text.length() > 1) {
+            int i = 0;
+            while (copy.length() > 0) {
+                if (copy.length() == 1) {
+                    System.out.print(text.charAt(i));
+                    break;
+                }
+                System.out.print(text.charAt(i) + "" + separator);
+                i++;
+                copy = copy.substring(0, copy.length() - 1);
+            }
+            System.out.println();
+        }
     }
+
 
     private static void addSeparator(int number, char separator) {
         //TODO: Implementieren Sie hier Ihre Lösung für die Angabe
@@ -33,18 +48,18 @@ public class Aufgabe2 {
         addSeparator(text2, ':');
         addSeparator(text3, '-');
         addSeparator(text4, '+');
-
-        addSeparator(1, '$');
-        addSeparator(35, '*');
-        addSeparator(657, ':');
-        addSeparator(2048, '#');
-        addSeparator(26348, '+');
-
-        addSeparator(text1, "+#$");
-        addSeparator(text2, ":*&!");
-
-        addSeparator(text0);
-        addSeparator(text1);
-        addSeparator(text2);
+//
+//        addSeparator(1, '$');
+//        addSeparator(35, '*');
+//        addSeparator(657, ':');
+//        addSeparator(2048, '#');
+//        addSeparator(26348, '+');
+//
+//        addSeparator(text1, "+#$");
+//        addSeparator(text2, ":*&!");
+//
+//        addSeparator(text0);
+//        addSeparator(text1);
+//        addSeparator(text2);
     }
 }
