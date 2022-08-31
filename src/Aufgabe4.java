73d6c1775063343d4e46ecc3828ae11ffcec3595
Mehmed Maljoki <mehmed.maljoki@porscheinformatik.com>
2022-09-01 01:51:11 +0200

    adding the first method imperative and recursive from Aufgabe4.java

--- src/Aufgabe4.java
+++ src/Aufgabe4.java
@@ -1,28 +1,54 @@
 /*
     Aufgabe 4) Rekursion
 */
+// it is not allowed to use helper functions and no globale variables
 public class Aufgabe4 {
 
-    private static int countCharsSmaller(String text, char value) {
-        // TODO: Implementieren Sie hier Ihre Lösung für die Methode
-        return -1;
-    }
+	// imperative
+//	private static int countCharsSmaller(String text, char value) {
+//		// TODO: Implementieren Sie hier Ihre Lösung für die Methode
+//		int count = 0;
+//		String textCopy = text;
+//		int i = 0;
+//		while (textCopy.length() > 0) {
+//			char ch = text.charAt(i);
+//			if (ch < value) {
+//				count++;
+//			}
+//			i++;
+//			textCopy = textCopy.substring(0, textCopy.length() - 1);
+//		}
+//		return count;
+//	}
 
-    private static String removeCharsInString(String text, char start, char end) {
-        // TODO: Implementieren Sie hier Ihre Lösung für die Methode
-        return null; //Zeile kann geändert oder entfernt werden.
-    }
+	// rekursiv
+	private static int countCharsSmaller(String text, char value) {
+		if (text.length() > 0) {
+			char ch = text.charAt(text.length() - 1);
+			if (ch < value) {
+				return 1 + countCharsSmaller(text.substring(0, text.length() - 1), value);
+			} else {
+				return countCharsSmaller(text.substring(0, text.length() - 1), value);
+			}
+		}
+		return 0;
+	}
 
-    private static String shiftDigitRight(String text) {
-        // TODO: Implementieren Sie hier Ihre Lösung für die Methode
-        return null; //Zeile kann geändert oder entfernt werden.
-    }
+	private static String removeCharsInString(String text, char start, char end) {
+		// TODO: Implementieren Sie hier Ihre Lösung für die Methode
+		return null; //Zeile kann geändert oder entfernt werden.
+	}
 
-    public static void main(String[] args) {
-        System.out.println(countCharsSmaller("DAS (ist) ]ein] Test!", (char) 100));
-        System.out.println(countCharsSmaller("a!", (char) 200));
-        System.out.println(countCharsSmaller("Ein Test", (char) 100));
-        System.out.println();
+	private static String shiftDigitRight(String text) {
+		// TODO: Implementieren Sie hier Ihre Lösung für die Methode
+		return null; //Zeile kann geändert oder entfernt werden.
+	}
+
+	public static void main(String[] args) {
+		System.out.println(countCharsSmaller("DAS (ist) ]ein] Test!", (char) 100));
+		System.out.println(countCharsSmaller("a!", (char) 200));
+		System.out.println(countCharsSmaller("Ein Test", (char) 100));
+		System.out.println();
 
 //        System.out.println(removeCharsInString("testtrompete", 'd', 'n'));
 //        System.out.println(removeCharsInString("test", 's', 'u'));
@@ -52,5 +78,5 @@ public class Aufgabe4 {
 //        assert (shiftDigitRight("4").equals("4"));
 //        assert (shiftDigitRight("ji)oiepk(2").equals("ji)oiepk(2"));
 //        assert (shiftDigitRight("ohneziffer").equals("ohneziffer"));
-    }
+	}
 }
